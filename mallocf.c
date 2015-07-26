2e8c5d7a33b158e5364e1c0a93a5e5613184f7cd
semitrivial <alexander@math.ohio-state.edu>
2015-07-26 08:31:03 +0100

    Move stdarg #include back to mallocf.h

--- mallocf.c
+++ mallocf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>//vsprintf and vsnprintf
 #include <stdlib.h>//size_t, malloc
-#include <stdarg.h>//va_start, va_end, va_list
 #include "mallocf.h"
 
 char *strdupf( const char *fmt, ... )
